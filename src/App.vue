6982e9e9d9d982294e75b85dea75524ba4154f94
PapaTobi <95914053+PapaTobi@users.noreply.github.com>
2022-01-09 16:11:57 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,7 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
+      "mission_slug": "002",
       "current_md": "",
       "events": "",
       "missions": [
@@ -102,6 +102,11 @@ export default {
           "slug": "001",
           "name": "Bug-Hunt",
           "status": "success"
+        },
+        {
+          "slug": "002",
+          "name": "Vigilant Gaze",
+          "status": "start"
         },
       ],
       "pilots": [
