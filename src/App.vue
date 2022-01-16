25985cbcdc5b37dcc3cb9fef22765a394eafda66
PapaTobi <95914053+PapaTobi@users.noreply.github.com>
2022-01-15 21:49:22 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,7 @@ export default {
         {
           "slug": "002",
           "name": "Vigilant Gaze",
-          "status": "start"
+          "status": "success"
         },
       ],
       "pilots": [
